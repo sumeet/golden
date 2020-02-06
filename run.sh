567f535b87a8d2e12c354e3161aae50052f90c12
Sumeet Agarwal <sumeet.a@gmail.com>
2020-02-06 14:31:44 +0100

    kill child processes from run.sh (issue #1)

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# from https://stackoverflow.com/a/22644006/149987
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
 # grabbed this badboy from https://stackoverflow.com/a/246128/149987
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
